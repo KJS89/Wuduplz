425a50df47ac985fe01e2b5a12a239670fea61ba
levi0910 <77359676+levi0910@users.noreply.github.com>
2021-02-21 16:20:39 -0500

    Add files via upload

---
+++ Server/update/route/front-end/action/comment/content.js
@@ -0,0 +1,27 @@
+const connection = require('../../../../mysql/actions/connection')
+var os = require('os');
+var networkInterfaces = os.networkInterfaces();
+
+
+
+module.exports = async(req,res)=>{
+    var videoId = req.params.videoId
+    let sql = `select user.UserName as username, user.PhotoPath as photopath,comment.CommentContent as content,
+                comment.CommentDate as commentdate,comment.ThumberNumber as thumbers
+                from comment 
+                inner join user
+                on comment.CommentatorId = user.UserId
+                where VideoId = '${videoId}'`
+    connection.query(sql,(err,result)=>{
+   
+        result.forEach((value,index)=>{
+           value['BaseUri']=networkInterfaces['WLAN'][1]['address']+':3000'
+           if(value['photopath']!=null)
+            value['photopath']=value['photopath'].split('|').join('//')
+           //     console.log(item)
+        })
+        //console.log(result)
+        res.send(result)
+    })
+    
+}
