cfb14a38ff620f45e14f6ce3b93dc87efd8c60ec
KJS89 <zwang374@uottawa.ca>
2021-05-01 00:36:46 -0400

    Age button

--- TestProject/src/Ingrid/signup-final version.js
+++ TestProject/src/Ingrid/signup-final version.js
@@ -10,6 +10,7 @@ import {
 
 const AddRequest = ({ navigation }) => {
   const [value, onChangeText] = React.useState('');
+  
 
   return (
 
@@ -17,7 +18,7 @@ const AddRequest = ({ navigation }) => {
 
     <Text style={styles.step}> Email </Text>
       <TextInput
-        style={{ height: 30, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
+        style={{ height: 40, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
         onChangeText={text => onChangeText(text)}
         placeholder={'Your email'}
         value={value}
@@ -25,26 +26,22 @@ const AddRequest = ({ navigation }) => {
 
     <Text style={styles.step}> Password </Text>
       <TextInput
-        style={{ height: 30, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
+        style={{ height: 40, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
         onChangeText={text => onChangeText(text)}
         placeholder={'Set your password'}
         value={value}
-      /> 
-      <TextInput
-        style={{ height: 30, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
-        onChangeText={text => onChangeText(text)}
-        placeholder={'Password confirm'}
-        value={value}
-      /> 
+      />  
     
 
-    <Text style={styles.step}> Nick Name </Text>
+    <Text style={styles.step}> User Name </Text>
       <TextInput
-        style={{ height: 30, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
+        style={{ height: 40, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
         onChangeText={text => onChangeText(text)}
-        placeholder={'Nick Name'}
+        placeholder={'User name'}
         value={value}
       /> 
+
+
 
     <Text style={styles.step}> GPS </Text>
     <Text style={styles.text}> Would you like to turn on your GPS </Text>
@@ -62,43 +59,29 @@ const AddRequest = ({ navigation }) => {
     </TouchableOpacity>
     </View>
     <Text style={styles.text}> Or you can type your city and country </Text> 
-
-
-    <View style={{flexDirection:'row', alignItems:'baseline',}}> 
-        <TextInput style={styles.subTextinStyle} placeholder={'City'} underlineColorAndroid={'transparent'} keyboardType={'numeric'} />
-        <TextInput style={styles.subTextinStyle} placeholder={'Province'} underlineColorAndroid={'transparent'} keyboardType={'numeric'} />
-        <TextInput style={styles.subTextinStyle} placeholder={'Country'} underlineColorAndroid={'transparent'} keyboardType={'numeric'} />
-        </View>
-
-    <Text style={styles.step}> Age </Text>
-
-
-    <Text style={styles.text}> Are you 18 years old </Text>
-    <TouchableOpacity activeOpacity={0.5}>
-          <View style={styles.AgeBottom}>
-            <Text style={styles.AgeBottomTxt}>Yes</Text>
-          </View>
-    </TouchableOpacity>
-
     <TextInput
-        style={{ height: 30, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
+        style={{ height: 40, borderColor: 'blue', borderWidth: 1, margin: 10, width: Dimensions.get('window').width - 30 }}
         onChangeText={text => onChangeText(text)}
-        placeholder={'Your year of birth'}
+        placeholder={'Your city and country'}
         value={value}
       />
 
-      <Text style={styles.step}> Please type 3 keywords </Text>
-        <View style={{flexDirection:'row', alignItems:'baseline',}}> 
-        <TextInput style={styles.subTextinStyle} placeholder={'Keyword 1'} underlineColorAndroid={'transparent'} keyboardType={'numeric'} />
-        <TextInput style={styles.subTextinStyle} placeholder={'Keyword 2'} underlineColorAndroid={'transparent'} keyboardType={'numeric'} />
-        <TextInput style={styles.subTextinStyle} placeholder={'Keyword 3'} underlineColorAndroid={'transparent'} keyboardType={'numeric'} />
-        </View>
-      
-      <TouchableOpacity activeOpacity={0.5}>
-        <View style={styles.keywordBtn}>
-          <Text style={styles.AgeBottomTxt}>For more keywords click here</Text>
-        </View>
-      </TouchableOpacity>
+    <Text style={styles.step}> Age </Text>
+    <Text style={styles.text}> Are you 18 years old </Text>
+    <View style={{flexDirection:'row', alignItems:'baseline',}}> 
+
+    <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.GPSbottonON}>
+            <Text style={styles.GPSbottonTxt}>ON</Text>
+          </View>
+    </TouchableOpacity>
+    <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.GPSbottonOFF}>
+            <Text style={styles.GPSbottonTxt}>OFF</Text>
+          </View>
+    </TouchableOpacity>
+    </View>
+
 
     <TouchableOpacity activeOpacity={0.5}>
           <View style={styles.startBottom}>
@@ -122,7 +105,7 @@ const styles = StyleSheet.create({
 
   step:{
     color:'#2B49C1',
-    fontSize:18,
+    fontSize:20,
   },
 
   text:{
@@ -136,39 +119,39 @@ const styles = StyleSheet.create({
   },
   
   GPSbottonON:{
-    width:'70%',
-    height: 25,
+    width:'80%',
+    height: 30,
     backgroundColor:'#5858FA',
     marginHorizontal:10
   },
   GPSbottonOFF:{
-    width:'70%',
-    height: 25,
+    width:'80%',
+    height: 30,
     backgroundColor:'#5858FA',
     marginHorizontal:10,
   },
   GPSbottonTxt:{
     color:'white',
     alignSelf:'center', 
-    fontSize: 15,
+    fontSize: 18,
     padding:5,
   },
   AgeBottom:{
-    width:'70%', 
-    height: 25,
+    width:50, 
+    height: 30,
     backgroundColor:'#5858FA',
     marginHorizontal:10
   },
   AgeBottomTxt:{
     color:'white',
     alignSelf:'center', 
-    fontSize:20,
+    fontSize:20
   },
 
 
   startBottom:{
     width:200,
-    height:40,
+    height:50,
     backgroundColor:'blue',
     alignItems:'center',
     justifyContent:'center',
@@ -182,21 +165,10 @@ const styles = StyleSheet.create({
     alignItems:'center',
     fontSize:20,
   },
-  subTextinStyle: {
-    width: '20%',
-    height:30,
-    marginLeft:15,
-    borderColor: 'blue',
-    borderWidth: 1, 
-    margin: 10, 
-  },
-  keywordBtn:{
-    width:'100%',
-    height: 25,
-    backgroundColor:'#5858FA',
-    marginHorizontal:10,
-  }
-  
 });
 
+
+
 export default AddRequest;
+
+
